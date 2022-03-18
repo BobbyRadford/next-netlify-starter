f2d1eea0ee1681c2589367308978b6579939ed6c
Bobby Radford <bobby@rownd.io>
2022-03-18 13:12:26 -0400

    feat: add RowndProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '@styles/globals.css'
+import { RowndProvider } from '@rownd/react'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <RowndProvider appKey='1335b068-d0ea-4c23-bbd8-8351ff4d4094'>
+      <Component {...pageProps} />
+    </RowndProvider>
+  )
 }
 
 export default Application
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,55 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useRownd } from '@rownd/react'
+import { useEffect } from 'react';
 
 export default function Home() {
+  const { is_initializing, user } = useRownd();
+
+  const defaultUserData = {
+    first_name: 'First Name',
+    last_name: 'Last Name',
+  }
+
+  useEffect(() => {
+    if (is_initializing) {
+      return
+    }
+    Object.entries(defaultUserData).forEach(([key, value]) => {
+      if (user.data[key] === undefined) {
+        window.rownd.user.setValue(key, value)
+      }
+    })
+  }, [is_initializing])
+  
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+    {!is_initializing && (
+      <div className="container">
+        <Head>
+          <title>Next.js Starter!</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+        <main>
+          <Header title={`Welcome to my app${user.data.first_name ? ', ' + user.data.first_name : ''}!`} />
+          <div>
+            <p>What's your name?</p>
+            <input
+              type="text"
+              defaultValue={!is_initializing ? user.data.first_name : ''}
+              onChange={(e) => {
+                window.rownd.user.setValue('first_name', e.target.value);
+              }}
+              />
+          </div>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    )}
+    </>
   )
 }
