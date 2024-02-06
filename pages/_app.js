80035b2bc02846d0b4a8185187d20ae0abb2c19e
Bobby Radford <bobby@rownd.io>
2024-02-06 12:35:04 -0500

    Add Red variant app key

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { RowndProvider } from '@rownd/react'
 
 function Application({ Component, pageProps }) {
   return (
-    <RowndProvider appKey='key_t7oxljlghf1h4zb7ey37f5cx' apiUrl='https://api.us-east-2.dev.rownd.io'>
+    <RowndProvider appKey='key_flttazhvqb4a9qn8yukfyekm' apiUrl='https://api.us-east-2.dev.rownd.io'>
       <Component {...pageProps} />
     </RowndProvider>
   )
