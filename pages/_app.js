d5a3f87d289af7ed172060ce31bb87d742c550e9
Bobby Radford <bobby@rownd.io>
2024-02-06 12:57:18 -0500

    Add base app key

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { RowndProvider } from '@rownd/react'
 
 function Application({ Component, pageProps }) {
   return (
-    <RowndProvider appKey='key_t7oxljlghf1h4zb7ey37f5cx' apiUrl='https://api.us-east-2.dev.rownd.io'>
+    <RowndProvider appKey='key_omgrqe4ttf2mqdpd1agkya6w' apiUrl='https://api.us-east-2.dev.rownd.io'>
       <Component {...pageProps} />
     </RowndProvider>
   )
